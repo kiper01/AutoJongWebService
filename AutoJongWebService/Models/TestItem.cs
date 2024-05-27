187102e8ad62c21c01864fca86c006d7dde2a07e
kiper01 <kiper0328@mail.ru>
2024-05-27 19:20:23 +1000

    Добавьте файлы проекта.

---
+++ AutoJongWebService/Models/TestItem.cs
@@ -0,0 +1,35 @@
+﻿namespace AutoJongWebService.Models
+{
+    public class Test
+    {
+        public uint StartYear { get; set; }
+        public uint StartPrice { get; set; }
+        public uint EndPrice { get; set; }
+        public FuelType Fuel { get; set; }
+        public double StartEngineVolume { get; set; }
+        public GearboxType Gearbox { get; set; }
+        public CountryType Country { get; set; }
+
+        public enum FuelType
+        {
+            Petrol,
+            Diesel,
+            Electric,
+            Hybrid
+        }
+
+        public enum GearboxType
+        {
+            Manual,
+            Automatic,
+            SemiAutomatic
+        }
+
+        public enum CountryType
+        {
+            Japan,
+            Korea,
+            China
+        }
+    }
+}
